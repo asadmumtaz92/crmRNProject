d00504dd7d60a8e299770d950225ac7c5e306eef
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-04-04 00:02:27 +0500

    This commit for add components and implement search functionality...

--- app/components/peopleList.js
+++ app/components/peopleList.js
@@ -1,100 +1,159 @@
-import React from "react";
+import React from "react"
 import {
-    TouchableOpacity,
     StyleSheet,
     FlatList,
     Image,
     View,
     Text,
-    Dimensions,
 } from 'react-native'
 
 import { Colors } from '../styles/color'
 
-const deviceWidth = Dimensions.get('window').width
-const PeopleList = ( props ) => {
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
-    // console.log("props--", props)
-    
+const PeopleList = ( props ) => {
 
     const renderItem = (items) => {
         let item = items.item
 
         return (
             <View key={item.id} style={styles.card}>
-                <View style={styles.imageBox}>
-                    {item.image
-                        ? <Image source={{uri: item?.image}} style={styles.image}  resizeMode="cover"/>
-                        : <Text style={styles.imageText}>
-                            {`${item?.firstname.substring(0, 1)}${item?.lastname.substring(0, 1)}`}
+                <View style={styles.topBox}>
+                    <View>
+                        {item.image
+                            ? <Image source={{uri: item?.image}} style={styles.image} resizeMode="cover" />
+                            : <View style={styles.imageTextBox}>
+                                <Text style={styles.imageText}>
+                                    {`${item?.firstname.substring(0, 1)}${item?.lastname.substring(0, 1)}`}
+                                </Text>
+                            </View>
+                        }
+                    </View>
+
+                    <View style={styles.nameBox}>
+                        <Text style={styles.name}>
+                            {item?.firstname} {item?.lastname}
                         </Text>
-                    }
+                        <Text style={styles.desig}>
+                            {item?.desig}
+                        </Text>
+                    </View>
                 </View>
 
-                <View style={{ alignContent: 'center', marginLeft:10, flexDirection: 'column' }}>
-                    <Text style={{}}>
-                        {item?.firstname} {item?.lastname}</Text>
-                    <Text style={{}}>{item?.company}</Text>
+                <View style={styles.bottomBox}>
+                    <FontAwesome5 name='building' style={styles.icons} />
+                    <Text style={styles.company}>{item?.company}</Text>
                 </View>
             </View>
         )
     }
 
     return (
-        <View style={styles.container}>
-            <FlatList
-                data={props.data}
-                contentContainerStyle={styles.flatlist}
-                keyExtractor={(item) => item.id }
-                renderItem={renderItem}
-            />
-        </View>
+        <FlatList
+            data={props.data}
+            contentContainerStyle={styles.flatlist}
+            keyExtractor={(item) => item.id }
+            renderItem={renderItem}
+            // bounces={false}
+            showsVerticalScrollIndicator={false}
+        />
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 26,
-        paddingVertical: 15,
-        flex: 1,
-    },
     flatlist: {
-        // 
+        paddingBottom: 10,
+        marginBottom: 10,
     },
     card: {
         backgroundColor: Colors.white,
-        // width: deviceWidth - 52,
-        alignContent: 'center',
-        // alignSelf: 'center',
-        flexDirection: 'row',
-        paddingVertical: 8,
-        marginVertical: 8,
+        flexDirection: 'column',
+        marginHorizontal: 5,
+        borderRadius: 10,
+        marginTop: 20,
+
+        shadowColor: Colors.primery,
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        shadowOffset: {
+            height: 0,
+            width:0,
+        },
         elevation: 3,
     },
-    imageBox: {
-        borderColor: Colors.gray,
-        justifyContent: 'center',
+    topBox: {
         alignContent: 'center',
-        borderRadius: 25,
-        borderWidth: 2,
-        height: 50,
-        width: 50,
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 10,
     },
     image: {
-        borderColor: Colors.primery,
-        borderRadius: 25,
-        borderWidth: 2,
-        height: 50,
-        width: 50,
+        borderRadius: 8,
+        height: 90,
+        width: 90,
+    },
+    imageTextBox: {
+        backgroundColor: Colors.lightgray,
+        justifyContent: 'center', 
+        alignItems: 'center',
+        borderRadius: 8,
+        height: 90,
+        width: 90,
+
     },
     imageText: {
         textTransform: 'uppercase',
         alignSelf: 'center',
-        textAlign: 'center',
+        fontWeight: '700',
+        fontSize: 30,
+
+    },
+    nameBox: {
+        marginLeft: 15,
+    },
+    name: {
+        textTransform:'capitalize',
         fontWeight: '700',
         fontSize: 22,
     },
-    imageAltBox: {},
+    desig: {
+        color: '#00000088', // Colors.gray,
+        textAlign: 'left',
+        letterSpacing: 0.7,
+        fontWeight: '400',
+        fontSize: 15,
+    },
+    bottomBox: {
+        flexDirection: 'row',
+        backgroundColor: Colors.primery,
+        borderBottomStartRadius: 10,
+        borderBottomEndRadius: 10,
+        justifyContent: 'center',
+        paddingVertical: 10,
+
+        shadowColor: Colors.primery,
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        shadowOffset: {
+            height: 0,
+            width: 0,
+        },
+        elevation: 3,
+    },
+    icons: {
+        color: Colors.white,
+        alignSelf: 'center',
+        marginRight: 8,
+        fontSize: 18,
+    },
+    company: {
+        textTransform: 'uppercase',
+        color: Colors.white,
+        textAlign: 'center',
+        letterSpacing: 0.7,
+        fontWeight: '600',
+        fontSize: 16,
+    },
 })
 
 export default PeopleList
